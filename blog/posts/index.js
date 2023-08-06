16c753c69be7aa9ce652b51b5a479ce44885e651
TecLegacy <kshvkumar.kk2@gmail.com>
2023-08-06 15:43:20 +0530

    cors policy

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
 const randomBytes = require('randombytes');
 const bodyParser = require('body-parser');
 
+app.use(cors())
 // raw-json
 app.use(bodyParser.json());
 
 // In memory -> id : hexId , title : string
 const post = {};
+
+// @GET /posts -> show all posts
+app.get('/posts', (req, res) => {
+  res.send(post);
+});
 
 // @POST /posts -> store post
 app.post('/posts', (req, res) => {
