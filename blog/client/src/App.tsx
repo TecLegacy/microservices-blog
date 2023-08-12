2339a8d74744a06775c38bf3d1c3b444a7525ebe
TecLegacy <kshvkumar.kk2@gmail.com>
2023-08-12 21:03:17 +0530

    Working With WSL 2 & gitlens in VS Code

--- blog/client/src/App.tsx
+++ blog/client/src/App.tsx
@@ -1,8 +1,10 @@
-import CreatePosts from '@/components/posts/Index';
-
+import CreatePosts from './components/posts/Index';
 function App() {
   return (
     <>
+      <h1 className='underline text-3xl font-bold '>
+        Working With WSL 2 & gitlens in VS Code
+      </h1>
       <div>
         <CreatePosts />
       </div>
