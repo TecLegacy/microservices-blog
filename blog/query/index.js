45811aa5e14de8779bd7a436d9e17cb466c9ee44
TecLegacy <kshvkumar.kk2@gmail.com>
2023-08-18 14:59:06 +0530

    Eventbus data store

--- blog/query/index.js
+++ blog/query/index.js
@@ -2,22 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { default: axios } = require('axios');
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // query service will listen for events from event bus
 // and store them in memory
-
 const postsAndComments = {};
 
-app.get('/posts', (req, res) => {
-  res.send(postsAndComments);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+// helper function to get all comments from event bus after query service is restarted/down
+const helperEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     postsAndComments[id] = { id, title, Comments: [] };
@@ -43,10 +38,33 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+//Routes handlers
+app.get('/posts', (req, res) => {
+  res.send(postsAndComments);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  helperEvent(type, data);
 
   res.send({ status: 'OK' });
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log(`Query service listening on port 4002`);
+
+  // get all events from event bus after query service is restarted/down
+  try {
+    const res = await axios.get('http://localhost:4006/events');
+
+    for (let event of res.data) {
+      console.log('Processing event:', event.type);
+      helperEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
