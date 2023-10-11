0d4d4f5f063cf09d0ebe8693dca90de8cbb0c189
TecLegacy <kshvkumar.kk2@gmail.com>
2023-10-11 17:56:21 +0530

    Reuseable Typesafe Button

--- ticketing/client/src/app/page.tsx
+++ ticketing/client/src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image';
+import Button from '@/components/ui/Button';
 
 export default function Home() {
   return (
     <>
       <h1>Hello</h1>
+      <h2>Landing Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        velit, nisi illum dolores culpa quae inventore rerum placeat voluptatem
+        incidunt impedit minus totam, praesentium rem asperiores iure quibusdam
+        illo corrupti.
+      </p>
+      <div className=" bg-white ">check </div>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+
+      <Button> w </Button>
     </>
   );
 }
