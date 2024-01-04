aad9132d0643ea87ac3c3f5bb4cef541a44c96eb
TecLegacy <kshvkumar.kk2@gmail.com>
2024-01-04 05:33:53 +0530

    Feat(shadcnUI)
    Added & tested shadcn

--- ticketing/client/src/app/layout.tsx
+++ ticketing/client/src/app/layout.tsx
@@ -1,9 +1,13 @@
-import ToastProvider from '@/components/TostProvider';
+import { Toaster } from '@/components/ui/sonner';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ToastProvider>{children}</ToastProvider>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   );
--- ticketing/client/src/app/page.tsx
+++ ticketing/client/src/app/page.tsx
@@ -1,25 +1,26 @@
-// 'use client';
-import Button from '@/components/ui/Button';
+'use client';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
 
-import toast from 'react-hot-toast';
 export default function Home() {
-  // const notify = () => toast('Here is your toast.');
   return (
     <>
       <h1>Hello</h1>
-      <h2>Landing Page</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        velit, nisi illum dolores culpa quae inventore rerum placeat voluptatem
-        incidunt impedit minus totam, praesentium rem asperiores iure quibusdam
-        illo corrupti.
-      </p>
-      <div className=" bg-white ">check </div>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        ...
-      </button>
 
-      {/* <Button onClick={notify}> w </Button> */}
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast('Event has been created', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+              label: 'Undo',
+              onClick: () => console.log('Undo'),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
     </>
   );
 }
